8272f4add42ee3223b64a7ea6851eb17815c4814
eduardo <eduardooost@gmail.com>
2024-12-02 09:48:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.StaticFiles;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.UseStaticFiles();
+
+// app.UseHttpsRedirection();
+
+app.MapGet("/", () =>
+{
+   string html = $@"
+      <!DOCTYPE html>
+      <html lang='en'>
+         <head>
+            <meta charset='utf-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <meta name='description' content='Test api'>                                                                                                        
+            <meta name='theme-color' content='#fff'>
+            <meta name='author' content='Eduardo Osteicoechea'>
+            <meta name='robots' content='index,follow'>
+            <link rel='stylesheet' href='index.css'>
+            <link rel='icon' type='image/x-icon' href='favicon.webp'>
+         </head>                                                                                                                                             
+      <body>                                                                                                                                                      
+         <main>
+            <h1>eomserver</h1>
+            <h2>We are preparing your user experience</h2>
+            <h3>Please wait</h3>                                                                                                                        
+         </main>                                                                                                                                             
+         <script src='index.js'></script>                                                                                                            
+      </body>                                                                                                                                     
+      </html> 
+   ";
+   
+   return Results.Content(html, "text/html");
+});
+
+//app.MapGet("/static/{*path}", (string path) =>
+//{
+//    return PhysicalFile("wwwroot/" + path, "text/plain");
+//});
+
+
+
+app.Run();
+
